a43b9053411bb0137f858b3fcb7a8fc05bd2ca74
satoshi <sashafem@mail.ru>
2023-08-28 16:13:32 +0700

    Классы и дженерики - правки

--- 7-swap/swap.ts
+++ 7-swap/swap.ts
@@ -1,9 +1,19 @@
-function swapKeysAndValues(obj: Record<string, number>): Record<number, string> {
-	const result: Record<number, string> = {}
-	for (const [key, value] of Object.entries(obj)) {
+function swapKeysAndValues<T extends {}>(obj: T): {[K in keyof T as T[K]]: K} {
+	const result: any = {}
+
+	for (const [key, value] of Object.entries<string | number>(obj)) {
 		result[value] = key
 	}
 
 	return result
 }
 
+const obj = {
+	name: 'Aleksandr',
+	age: 22,
+	role: 'Admin'
+} as const
+
+const pickedObj = swapKeysAndValues(obj)
+
+pickedObj.Admin
